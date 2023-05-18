667f2b56bcd473ccd934e703b24ad62f0ec2c3bf
Gilnathan Reis <102397101+GinathanReis@users.noreply.github.com>
2023-05-18 17:25:51 -0300

    Alteração: CSS , JS
    Criaçao de uma interface de navegação que foi projetado com um layout limpo e organizado, facilitando a localização dos itens de navegação.

--- script.js
+++ script.js
@@ -19,3 +19,72 @@ const ctx = document.getElementById('myChart');
     }
   });
 
+/*  Botão para abri a aba deshbor   */
+
+let dashboard = window.document.getElementById('dashboard')
+let bnt_dash = window.document.getElementById('bnt_dashboard')
+let imagem_dashboard = window.document.getElementById('imagem_dashboard')
+let h2_bnt_dashboard = window.document.getElementById('h2_bnt_dashboard')
+
+let bntINS = window.document.getElementById('bntINS')
+let imagemInst = window.document.getElementById('imagemInst')
+let H2inst = window.document.getElementById('H2inst')
+
+let bntKPIS = window.document.getElementById('bntKPIS')
+let imagemKPIS = window.document.getElementById('imagemKPIS')
+let h2_KPIS = window.document.getElementById('h2_KPIS')
+
+
+function bnt_dashboard(){
+  dashboard.style.display='block';
+  bnt_dash.style.background='#F2F2F2';
+  imagem_dashboard.style.filter = "none";
+  h2_bnt_dashboard.style.color ='#828284'
+
+  bntINS.style.background='none';
+  imagemInst.style.filter = 'brightness(0) invert(1)';
+  H2inst.style.color='white'
+
+  bntKPIS.style.background='none';
+  imagemKPIS.style.filter = 'brightness(0) invert(1)';
+  h2_KPIS.style.color='white';
+}
+
+/*  Botão para abri a aba instituitons   */
+
+
+
+function bnt_institutions(){
+  dashboard.style.display='none'
+  bnt_dash.style.background='none';
+  imagem_dashboard.style.filter = "brightness(0) invert(1)";
+  h2_bnt_dashboard.style.color ='white';
+  
+  bntINS.style.background='#F2F2F2';
+  imagemInst.style.filter = "none";
+  H2inst.style.color='#828284'
+
+  bntKPIS.style.background='none';
+  imagemKPIS.style.filter = 'brightness(0) invert(1)';
+  h2_KPIS.style.color='white';
+}
+
+
+/*  Botão para abri a aba KPIS   */
+
+function bnt_KPIs(){
+  dashboard.style.display='none'
+  bnt_dash.style.background='none';
+  imagem_dashboard.style.filter = "brightness(0) invert(1)";
+  h2_bnt_dashboard.style.color ='white';
+
+
+  bntINS.style.background='none';
+  imagemInst.style.filter = 'brightness(0) invert(1)';
+  H2inst.style.color='white'
+
+
+  bntKPIS.style.background='#F2F2F2';
+  imagemKPIS.style.filter = "none";
+  h2_KPIS.style.color='#828284';
+}
